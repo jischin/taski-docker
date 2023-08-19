220e846fe475750453db450090d66878d01245f7
Дмитрий Прокопенко <diozan@inbox.ru>
2023-08-19 16:54:24 +0300

    Поправил конфиги, сделал .env, исправил 2-ю ошибку

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,14 +1,14 @@
 import os
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 from pathlib import Path
 
-# load_dotenv()
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG') == 'True'
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split()
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 # Application definition
